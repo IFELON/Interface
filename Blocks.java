668b00fcf5c596622e1c01a6f72ad5f2ecd3a652
IFELON <babu.suri94@gmail.com>
2018-11-26 18:15:12 +0530

    Add files via upload
    This is the code

---
+++ Blocks.java
@@ -0,0 +1,13 @@
+package in.suriya.aishwaryainfra;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Blocks extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_block);
+    }
+}
